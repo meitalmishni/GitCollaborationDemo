63c5822a54eac7d1c4edf38f7750447f97340358
markv <markv@steps.me>
2023-01-14 10:02:38 +0200

    Add logs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function sum(arrayOfNumbers) {
     let sum = 0;
+    console.log('Calculating sum...');
     arrayOfNumbers.forEach((number) => sum+=number);
+    console.log('Sum calculated');
 }
 
 const arraySum = sum([1,2,3,4]);
